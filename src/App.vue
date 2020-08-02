df0754dee33f650f447d01208a6ab21233784b04
rixiobarrios <rixiobarrios.consulado@gmail.com>
2020-08-02 18:42:03 -0500

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Home msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Home from "./components/Home.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Home
   }
 };
 </script>
@@ -21,8 +20,11 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* text-align: center; */
+  color: #ffff;
+  /* margin-top: 60px; */
+}
+body {
+  background: black;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">HELLO!</div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
